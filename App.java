a113670d134d5e59bc9c0c88a2547bcc802295d5
alec2511 <98748402+alec2511@users.noreply.github.com>
2022-10-12 10:44:57 +0200

    Add files via upload

---
+++ App.java
@@ -0,0 +1,28 @@
+ package com.example;
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class App 
+{
+    public static void main( String[] args ) throws Exception
+    {
+        ServerSocket ss = new ServerSocket(3000);
+        System.out.println("Server in ascolto sulla porta 3000");
+     
+        boolean running=true;
+        while(running){
+            Socket s = ss.accept();
+        ClientHandler ch=new ClientHandler(s);
+        ch.start();
+
+        }
+      ss.close();     
+        
+    
+}
+}
